3f3caf00c20b8183817214dac8112624502a49fc
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-07 02:07:15 +0000

    agregue acordion

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,28 +1,29 @@
-import React,{useContext, useEffect} from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import "../../styles/card.css";
 
 export const Card = () => {
-    const {store,actions}=useContext(Context)
-    useEffect(()=>{
+    const { store, actions } = useContext(Context)
+    useEffect(() => {
         actions.loadSomePeople()
-    },[])
+    }, [])
     console.log(store.Character)
     return (
-        <div>
-            {store.Character.map((item,index)=>{            
-            <div key={index} className="card" style={{width: "18rem"}}>
-                <img src="https://lumiere-a.akamaihd.net/v1/images/obi-wan-kenobi-main_3286c63c.jpeg?region=0%2C0%2C1280%2C721" className="card-img-top" alt="..."/>
+        <div className="card-container">
+            {store.Character.map((item, index) => (
+                <div key={index} className="card" style={{ width: "18rem" }}>
+                    <img src="https://lumiere-a.akamaihd.net/v1/images/obi-wan-kenobi-main_3286c63c.jpeg?region=0%2C0%2C1280%2C721" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{item.name}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div>
-                        <Link to="#" className="btn btn-primary">Go somewhere</Link>
-                        <Link to="#" className="btn btn-primary"><i className="fa-regular fa-heart" style={{color: "#ffff00"}}></i></Link>
+                            <Link to="#" className="btn btn-primary">Go somewhere</Link>
+                            <Link to="#" className="btn btn-primary"><i className="fa-regular fa-heart" style={{ color: "#ffff00" }}></i></Link>
                         </div>
                     </div>
-            </div> 
-        })}
-        </div>       
+                </div>
+            ))}
+        </div>
     );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "../../styles/home.css";
-import {Card} from "../component/Card"
+import { Card } from "../component/Card"
 
 export const Home = () => (
 	<div className="container">
-		<Card/>
+		<div className="accordion accordion-flush" id="accordionFlushExample">
+			<div className="accordion-item">
+				<h1 className="accordion-header">
+					<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+						Characters
+					</button>
+				</h1>
+				<div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+					<div className="accordion-body"><Card/></div>
+				</div>
+			</div>
+			<div className="accordion-item">
+				<h1 className="accordion-header">
+					<button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+						Planets
+					</button>
+				</h1>
+				<div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+					<div className="accordion-body">tent.</div>
+				</div>
+			</div>
+		</div>
+		
 	</div>
 );
